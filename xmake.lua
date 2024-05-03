83a21fdff34eac8ff4cc2356a4a30a9325db141f
maxchen32 <max_chen32@foxmail.com>
2024-05-03 18:54:49 +0800

    updated xmake.io and workflow file

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,7 @@
 add_rules("mode.debug", "mode.release")
 
+includes("@builtin/xpack")
+
 if is_mode("debug") then
 	add_defines("DEBUG")
 	set_symbols("debug")
@@ -15,49 +17,59 @@ end
 target("objs")
 	set_kind("object")
 	set_warnings("all")
-	add_includedirs("include")
+	add_includedirs("include", {public = true})
 	add_files("src/*.c")
 	add_cflags("-fPIC")
 
-target("radicalite")
+target("radicalite_static")
 	set_kind("static")
 	set_warnings("all")
-	add_includedirs("include")
-	--add_files("src/*.c")
 	add_deps("objs")
 	set_basename("radical")
 	set_prefixname("lib")
+	set_targetdir("$(buildir)/lib")
 
 
 target("radicalite_shared")
 	set_kind("shared")
 	set_warnings("all")
-	add_includedirs("include")
-	--add_files("src/*.c")
 	add_deps("objs")
 	set_basename("radical")
 	set_prefixname("lib")
+	set_targetdir("$(buildir)/lib")
 
 target("headers")
 	set_kind("headeronly")
 	add_headerfiles("include/(*.h)") 
---local fs = os.filedirs("~/radicalite/*")
---print(fs[1])
 
-local tests = {"eyyc", "xyline", "sin-cos", "eyyc_new", "simple_vector_calcs"}
+local examples = {"eyyc", "xyline", "sin-cos", "eyyc_new", "simple_vector_calcs"}
 if is_mode("debug") then
-	table.insert(tests, "test")
+	table.insert(examples, "test")
 end
-for i,filename in pairs(tests) do
+for i,filename in pairs(examples) do
 	target(filename)
 		set_kind("binary")
+		add_files("examples/"..filename..".c")
 		set_default(false)
 		set_warnings("all")
-		add_includedirs("include")
-		add_deps("radicalite")
+		add_deps("radicalite_shared")
+		add_linkdirs("$(buildir)/lib")
+		add_links("radical")
 		add_rpathdirs(".")
 		add_rpathdirs("/usr/lib")
 		add_rpathdirs("/usr/local/lib")
 		add_rpathdirs("@executable_path")
-		add_files("examples/"..filename..".c")
+		set_targetdir("$(buildir)/examples")
 end
+
+xpack("pack")
+    set_formats("zip")
+	set_basename("radicalite-$(plat)")
+	add_targets("radicalite_static")
+	add_targets("radicalite_shared")
+	add_targets(examples)
+	add_targets("headers")
+	add_installfiles("(LICENSE)")
+	add_installfiles("(README.md)")
+	add_installfiles("(docs/**)")
+    
